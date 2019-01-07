7248b739dddbef9afdf9416ff7ff4ed67edac7ef
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2019-01-07 15:27:18 -0800

    added shutil import and code for copying predetermined files and folders

--- copy_paste.py
+++ copy_paste.py
@@ -5,3 +5,9 @@ Author: Lawrance Glenn
 Description: A simple program allowing user to move, copy and paste files with a single click.
 '''
 from maya import cmds
+import shutil, os
+files = ['file1.txt', 'file2.txt', 'file3.txt']
+for f in files:
+    shutil.copy(f, 'dest_folder')
+
+    ''' user interface still needed'''
